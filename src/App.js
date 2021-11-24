98003ecec2b55780683fbb995f02548e952e5cc3
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-11-24 15:08:11 +0000

    add sections

--- src/App.js
+++ src/App.js
@@ -18,6 +18,15 @@ import pieninyAnother from './pics/pieniny/p1.jpg'
 //Nature
 import n1 from './pics/nature/n1.jpeg'
 import n2 from './pics/nature/n2.jpeg'
+//Baltic
+import b1 from './pics/baltic/b1.jpg'
+import b2 from './pics/baltic/b2.jpg'
+//Sunsets
+import s1 from './pics/sunsets/s1.jpeg'
+import s2 from './pics/sunsets/s2.jpeg'
+//Wisla
+import w1 from './pics/wisla/w1.jpeg'
+import w2 from './pics/wisla/w2.jpeg'
 
 import {
   BrowserRouter as Router,
@@ -29,6 +38,9 @@ const sections = [
   {id:1, name: "Zakopane", img: {title: zakopaneTitle, another: zakopaneAnother}, desc: "Spontaniczny wyjazd do Zakopanego :)", slug: "zakopane"},
   {id:2, name: "Pieniny", img: {title: pieninyTitle, another: pieninyAnother}, desc: "Kilka ujęć z pięknych Pienin", slug: "pieniny"},
   {id:3, name: "Natura", img: {title: n1, another: n2}, desc: "", slug: "natura"},
+  {id:4, name: "Bałtyk", img: {title: b1, another: b2}, desc: "", slug: "baltic"},
+  {id:5, name: "Sunsets", img: {title: s1, another: s2}, desc: "", slug: "sunsets"},
+  {id:6, name: "Wisła", img: {title: w1, another: w2}, desc: "", slug: "wisla"},
 ]
 
 function App() {
