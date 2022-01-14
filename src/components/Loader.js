fde21c2b39c485d6a52eddfcdf53b2acf358b663
Michał Potoczny <michal.potoczny.92@gmail.com>
2022-01-14 11:46:02 +0100

    Update Loader.js

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,7 +3,7 @@ import jumpingKangaroo from "../pics/kangaroo.png";
 
 const Loader = () => {
   return (
-    <div className="w-full h-screen grid grid-cols-1 gap-4 place-content-center">
+    <div className="fixed bg-white left-0 top-0 w-full h-screen grid grid-cols-1 gap-4 place-content-center">
       <div className='mb-20'>
         <div className="flex justify-center animate-bounce">
           <img
