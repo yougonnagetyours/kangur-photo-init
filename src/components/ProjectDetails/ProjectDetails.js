7bc08e85e7bca16ed81cfde18a4e2dacbe37c737
Michał Potoczny <michal.potoczny.92@gmail.com>
2022-01-14 09:36:28 +0100

    Add loader hook

--- src/components/ProjectDetails/ProjectDetails.js
+++ src/components/ProjectDetails/ProjectDetails.js
@@ -1,34 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
 
 const ProjectDetails = ({ sections }) => {
   let { id } = useParams();
+  const [isLoaded, setIsLoaded] = useState(false);
 
   return (
     <>
-      {sections.map((section) =>
-        id === section.slug ? (
-          <div className="mt-14 mx-4 mb-8">
-            <div className="mb-10">
-              <p className="text-4xl">{section.name}</p>
-            </div>
-            <div className="mb-10">
-              <p className="text-base font-light text-justify">
-                {section.desc}
-              </p>
-            </div>
-            {section.imgs.map((img) => (
-              <div className="mb-10">
-                <img
-                  className="block w-full h-full object-cover"
-                  src={img}
-                  alt="candles"
-                />
+      {isLoaded
+        ? sections.map((section) =>
+            id === section.slug ? (
+              <div className="mt-14 mx-4 mb-8">
+                <div className="mb-10">
+                  <p className="text-4xl">{section.name}</p>
+                </div>
+                <div className="mb-10">
+                  <p className="text-base font-light text-justify">
+                    {section.desc}
+                  </p>
+                </div>
+                {section.imgs.map((img) => (
+                  <div className="mb-10">
+                    <img
+                      className="block w-full h-full object-cover"
+                      src={img}
+                      alt="candles"
+                    />
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
-        ) : null
-      )}
+            ) : null
+          )
+        : "Loading"}
     </>
   );
 };
